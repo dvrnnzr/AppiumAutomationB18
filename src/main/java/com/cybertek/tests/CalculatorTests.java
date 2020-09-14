322776859bc55ff5050964845b50a53383013800
dvrnnzr <dvrnnzr@gmail.com>
2020-09-14 16:59:40 -0400

    first test

--- src/main/java/com/cybertek/tests/CalculatorTests.java
+++ src/main/java/com/cybertek/tests/CalculatorTests.java
@@ -1,30 +1,40 @@
 package com.cybertek.tests;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+
 public class CalculatorTests {
+
    AndroidDriver<AndroidElement> driver;
+
    /**
     * {
-    *   "platformName": "Android",
-    *   "deviceName": "Pixel_2",
-    *   "appPackage": "com.android.calculator2",
-    *   "platformVersion": "7.0",
-    *   "fullReset": "true",
-    *   "automationName": "UiAutomator2",
-    *   "newCommandTimeout": "7200",
-    *   "appActivity": "com.android.calculator2.Calculator"
+    * "platformName": "Android",
+    * "deviceName": "Pixel_2",
+    * "appPackage": "com.android.calculator2",
+    * "platformVersion": "7.0",
+    * "fullReset": "true",
+    * "automationName": "UiAutomator2",
+    * "newCommandTimeout": "7200",
+    * "appActivity": "com.android.calculator2.Calculator"
     * }
     */
    @Before
-   public void setup() throws MalformedURLException {
-      //MobileCapabilityType.AUTOMATION_NAME=="platformName"
+   public void setup() throws Exception {
+//        CapabilityType.PLATFORM_NAME == "platformName"
+//        MobileCapabilityType.AUTOMATION_NAME == "automationName"
+//        AndroidMobileCapabilityType.APP_ACTIVITY == "appActivity"
 
       DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
       desiredCapabilities.setCapability("platformName", "Android");
@@ -35,10 +45,29 @@ public class CalculatorTests {
       desiredCapabilities.setCapability("newCommandTimeout", "7200");
       desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
       driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+      //http://localhost:4723/wd/hub - appium server address
    }
-   @Test
-   public void test1(){
-      System.out.println("test 1");
 
+   @Test
+   public void test1() {
+      System.out.println("Test 1");
+      AndroidElement btn2 = driver.findElementById("com.android.calculator2:id/digit_2");
+      AndroidElement equals = driver.findElementByAccessibilityId("equals");
+      AndroidElement plus = driver.findElementByAccessibilityId("plus");
+      AndroidElement result = driver.findElementById("com.android.calculator2:id/result");
+
+      btn2.click(); //2
+      plus.click(); //+
+      btn2.click(); //2
+      equals.click();// =
+
+      String actualResult = result.getText();
+
+      Assert.assertEquals("4", actualResult);
+   }
+
+   @After
+   public void tearDown(){
+      driver.closeApp();
    }
 }
